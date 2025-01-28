085b09f22dac2d5718f1f5ba05c5f7b3aa3ecdb9
eugenel97 <eugenejr14@gmail.com>
2025-01-27 16:37:35 -0800

    Bottom left is black due to (x,y) being both negative

--- shader.fs
+++ shader.fs
@@ -1,7 +1,11 @@
 #version 330 core
+
 out vec4 FragColor;
-in vec3 ourColor;
+
+//in vec3 ourColor;
+in vec3 positionToColor;
+
 void main()
 {
-   FragColor = vec4(ourColor, 1.0);
+   FragColor = vec4(positionToColor, 1.0);
 };
