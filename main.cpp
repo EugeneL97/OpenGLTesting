9e03faa77878f307b59f75304df38bf8fa835f4a
eugenel97 <eugenejr14@gmail.com>
2025-02-06 09:22:03 -0800

    Changing order of transformations leads to rotating around the translation (bottom right)

--- main.cpp
+++ main.cpp
@@ -154,8 +154,8 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture2);
 
         glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.5f));
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.5f));
 
         ourShader.use();
         unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
