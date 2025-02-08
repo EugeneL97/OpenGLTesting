bd2d53fc23d6ca5ee527c33dab83ac0c3d1a32e4
eugenel97 <eugenejr14@gmail.com>
2025-02-08 08:55:12 -0800

    Proper use of the z buffer to get the intended effect of a cube

--- main.cpp
+++ main.cpp
@@ -157,6 +157,8 @@ int main()
     }
     stbi_image_free(data);
 
+    glEnable(GL_DEPTH_TEST);
+
     ourShader.use();
 
     glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
@@ -166,7 +168,7 @@ int main()
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
