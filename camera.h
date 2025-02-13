d8feed2d0cbe77e4c8f083f21cae723ddaf9cb38
eugenel97 <eugenejr14@gmail.com>
2025-02-12 16:08:10 -0800

    True FPS camera is a one line fix

--- camera.h
+++ camera.h
@@ -69,6 +69,7 @@ public:
             Position -= Right * velocity;
         if (direction == RIGHT)
             Position += Right * velocity;
+        Position.y = 0.0f;
     }
     void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
     {
